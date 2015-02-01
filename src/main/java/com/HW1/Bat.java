20145ab812f65acad8ebc8a05675196ec40fc79e
Rohini Kulkarni <rohinikulkarni20@gmail.com>
2015-02-01 00:29:57 -0600

    Added Bat class

---
+++ src/main/java/com/HW1/Bat.java
@@ -0,0 +1,28 @@
+package com.HW1;
+
+public class Bat extends Creature implements Flyer{
+  private String name;
+
+  public Bat(String name) {
+    super(name);
+    this.name = name;
+  }
+
+  @Override
+  public void move() {
+    this.fly();
+  }
+
+  @Override
+  public void fly() {
+    System.out.println(name + " " + getClass().getSimpleName() + " is swooping through the dark.");
+  }
+
+  @Override
+  public void eat(Thing thing) {
+    if (thing instanceof Creature)
+      super.eat(thing);
+    else if (thing.getClass().equals(Thing.class))
+      System.out.println(name + " " + getClass().getSimpleName() + " won't eat a " + thing);
+  }
+}
---
+++ src/test/java/com/HW1/BatTest.java
@@ -0,0 +1,37 @@
+package com.HW1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BatTest extends BaseTest {
+  @Test
+  public void shouldReturnBatName() {
+    Bat bat = new Bat("Bat1");
+    assertEquals("Bat1 Bat", bat.toString());
+  }
+
+  @Test
+  public void shouldPrintFlyMoves() {
+    Bat bat = new Bat("Bat1");
+    bat.move();
+    assertEquals("Bat1 Bat is swooping through the dark.", out.toString().trim());
+  }
+
+  @Test
+  public void shouldNotEatThings() {
+    Thing thing = new Thing("Thing1");
+    Bat bat = new Bat("Bat1");
+    bat.eat(thing);
+    assertEquals("Bat1 Bat won't eat a Thing1", out.toString().trim());
+  }
+
+
+  @Test
+  public void shouldEatCreatureOnly() {
+    Tiger tiger = new Tiger("Tiger 1");
+    Bat bat = new Bat("Bat1");
+    bat.eat(tiger);
+    assertEquals("Bat1 has eaten a Tiger 1 Tiger", out.toString().trim());
+  }
+}
