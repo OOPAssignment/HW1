ea75aca7670b73b778849dd416a73fdba8d13f7f
Rohini Kulkarni <rohinikulkarni20@gmail.com>
2015-02-01 00:33:17 -0600

    Added acceptance test for subclasses

--- src/main/java/com/HW1/acceptance/TestCreature.java
+++ src/main/java/com/HW1/acceptance/TestCreature.java
@@ -1,9 +1,6 @@
 package com.HW1.acceptance;
 
-import com.HW1.Ant;
-import com.HW1.Creature;
-import com.HW1.Thing;
-import com.HW1.Tiger;
+import com.HW1.*;
 
 import java.util.ArrayList;
 import java.util.stream.IntStream;
@@ -28,5 +25,24 @@ public class TestCreature {
     creatures.add(new Ant("Tick Tock"));
     System.out.println("Creatures: ");
     creatures.stream().forEach(System.out::println);
+
+    Thing thing = new Thing("Thing 1");
+    Tiger tiger = new Tiger("Creature 1");
+    Ant ant = new Ant("Ant 1");
+
+    ant.eat(tiger);
+    ant.eat(thing);
+    ant.move();
+
+    Fly fly = new Fly("Fly 1");
+    fly.eat(thing);
+    fly.eat(tiger);
+    fly.move();
+
+    Bat bat = new Bat("Bat 1");
+    bat.eat(thing);
+    bat.eat(tiger);
+    bat.eat(ant);
+    bat.move();
   }
 }
