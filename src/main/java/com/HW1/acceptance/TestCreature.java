e5b0827c848c17d2f6b8a907ebfe8e8db80105e9
Rohini Kulkarni <rohinikulkarni20@gmail.com>
2015-01-31 23:44:36 -0600

    Added acceptance test

---
+++ src/main/java/com/HW1/acceptance/TestCreature.java
@@ -0,0 +1,22 @@
+package com.HW1.acceptance;
+
+public class TestCreature {
+
+    public TestCreature() {
+
+    }
+
+    public static final int THING_COUNT = 0;
+    public static final int CREATURE_COUNT = 0;
+
+    public static void main(java.lang.String[] args) {
+        System.out.println("hihiii");
+        System.out.println("hihiii");
+        System.out.println("hihiii");
+        System.out.println("hihiii");
+        System.out.println("hihiii");
+        System.out.println("hihiii");
+        System.out.println("hihiii");
+    }
+
+}
