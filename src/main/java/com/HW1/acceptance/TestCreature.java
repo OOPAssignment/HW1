135210f5b88159722819d1c2d208679ea99ad524
Rohini Kulkarni <rohinikulkarni20@gmail.com>
2015-02-01 00:12:16 -0600

    Added acceptance test for Tiger

--- src/main/java/com/HW1/acceptance/TestCreature.java
+++ src/main/java/com/HW1/acceptance/TestCreature.java
@@ -1,22 +1,24 @@
 package com.HW1.acceptance;
 
+import com.HW1.Thing;
+import com.HW1.Tiger;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 public class TestCreature {
 
     public TestCreature() {
 
     }
 
-    public static final int THING_COUNT = 0;
-    public static final int CREATURE_COUNT = 0;
+    public static final int THING_COUNT = 10;
+    public static final int CREATURE_COUNT = 5;
 
     public static void main(java.lang.String[] args) {
-        System.out.println("hihiii");
-        System.out.println("hihiii");
-        System.out.println("hihiii");
-        System.out.println("hihiii");
-        System.out.println("hihiii");
-        System.out.println("hihiii");
-        System.out.println("hihiii");
+        ArrayList<Thing> things = new ArrayList<>();
+        IntStream.range(0, 5).forEach(i -> things.add(new Thing("Thing" + i)));
+        IntStream.range(6, THING_COUNT).forEach(i -> things.add(new Tiger("Tiger" + i)));
+        things.stream().forEach(System.out::println);
     }
-
 }
